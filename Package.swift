706482484fe53be7419b01f9776cb1940450794b
Harri Hohteri <harri.hohteri@sportiq.xyz>
2022-11-30 09:58:26 +0200

    Make lib static

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "EFRSDK",
+            type: .static,
             targets: ["EFRSDK"])
     ],
     targets: [
@@ -45,8 +46,7 @@ let package = Package(
 		"Categories/NSString+SILBrowserNotifications.m",
 		"Models/SILOTAFirmwareFile.m",
 		"Models/SILWeakNotificationPair.m"
-	    ],
-	    publicHeadersPath: "EFRSDK"
+	    ]
         )
     ]
 )
