f32568583d3f5c96715524b19da9d0ac759c1fec
Harri Hohteri <harri.hohteri@sportiq.xyz>
2022-11-30 10:16:03 +0200

    Move headers

---
+++ SiliconLabsApp/include/EFRSDK.h
@@ -0,0 +1,12 @@
+//
+//  EFRSDK.h
+//  EFRSDK
+//
+//  Created by Harri Hohteri on 23.11.2022.
+//  Copyright © 2022 SiliconLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SILOTAFirmwareUpdateManager.h"
+#import "SILOTAFirmwareFile.h"
+#import "SILCentralManager.h"
